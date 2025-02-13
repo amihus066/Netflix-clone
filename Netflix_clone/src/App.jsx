1b6342f254b2f3b6ce1bcb5e428d5ae38b0e062b
amirr193 <amirr.husen193@gmail.com>
2025-02-13 01:51:42 -0500

    heder and footer

--- Netflix_clone/src/App.jsx
+++ Netflix_clone/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1> hello world</h1>
+      <Home />
     </>
   );
 }
