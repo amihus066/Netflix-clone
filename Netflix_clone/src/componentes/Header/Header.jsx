6bc91690433ffac4c99d1103fa083a3f67856ae3
amirr193 <amirr.husen193@gmail.com>
2025-02-14 20:54:57 -0500

    banner

--- Netflix_clone/src/componentes/Header/Header.jsx
+++ Netflix_clone/src/componentes/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./header.modules.css";
+import "./header.css";
 import NetflixLogo from "../../assets/images/Netflix-logo.jpg";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
