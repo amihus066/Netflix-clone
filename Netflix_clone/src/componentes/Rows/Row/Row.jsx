27b153a04b59e822821b77b6d72ce4c58b3bfdec
amirr193 <amirr.husen193@gmail.com>
2025-02-26 06:35:28 -0500

    rediploy

--- Netflix_clone/src/componentes/Rows/Row/Row.jsx
+++ Netflix_clone/src/componentes/Rows/Row/Row.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import "./Row.css";
+import React, { useEffect, useState } from "react";
 import axios from "../../utils/axios";
 import movieTrailer from "movie-trailer";
 import YouTube from "react-youtube";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./Row.css";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
@@ -14,12 +17,10 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
   useEffect(() => {
     (async () => {
       try {
-        console.log(fetchUrl);
         const request = await axios.get(fetchUrl);
-        console.log(request);
         setMovies(request.data.results);
       } catch (error) {
-        console.log("error", error);
+        console.log("Error fetching movies:", error);
       }
     })();
   }, [fetchUrl]);
@@ -28,20 +29,17 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      movieTrailer(movie?.title || movie?.name || movie?.original_name).then(
-        (url) => {
-          console.log(url);
+      movieTrailer(movie?.title || movie?.name || movie?.original_name)
+        .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
-          console.log(urlParams);
-          console.log(urlParams.get("v"));
           setTrailerUrl(urlParams.get("v"));
-        }
-      );
+        })
+        .catch((error) => console.log(error));
     }
   };
 
   const opts = {
-    hight: "390",
+    height: "390",
     width: "100%",
     playerVars: {
       autoplay: 1,
@@ -51,24 +49,38 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
   return (
     <div className="row">
       <h1>{title}</h1>
-      <div className="row__posters">
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={10}
+        slidesPerView={5} // Adjust slides per view
+        navigation
+        loop={true}
+        breakpoints={{
+          320: { slidesPerView: 2 },
+          640: { slidesPerView: 3 },
+          1024: { slidesPerView: 5 },
+        }}
+      >
         {movies?.map((movie, index) => (
-          <img
-            onClick={() => handleClick(movie)}
-            key={index}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            onError={(e) => (e.target.style.display = "none")}
-          />
+          <SwiperSlide key={index}>
+            <img
+              onClick={() => handleClick(movie)}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              onError={(e) => (e.target.style.display = "none")}
+            />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
 
-      <div style={{ padding: "40px" }}>
-        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-      </div>
+      {trailerUrl && (
+        <div style={{ padding: "40px" }}>
+          <YouTube videoId={trailerUrl} opts={opts} />
+        </div>
+      )}
     </div>
   );
 };
